154595fb794289d2ec31485eb4af20f28a03af6e
Wiam Malek <wiam.malek95@gmail.com>
2021-04-27 13:14:33 +0200

    Partie 2 Jquery

---
+++ exercice 7/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $(".a_supprimer").remove(); 
+ })
---
+++ exercice 9/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#texte_1, #texte_3").css({"border" : "5px green dashed"})
+    // $("#texte_1, #texte_3").css( "border","5px green dashed");
+
+  })
